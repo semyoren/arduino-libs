06b5addb1183ee513ceff009a083f67c1b7fba51
Semyon Kotliarenko <semako.ua@gmail.com>
2024-12-10 23:20:05 +0100

    wip

---
+++ ota_resolver/examples/prom_01_local/config.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID     ""
+#define WIFI_PASSWORD ""
+
+#define URL "IP_ADDRESS_OR_HOST_OF_SERVER" // "192.168.0.1" or "prometheus.yourdomain.local" No http or https, No port or path
+#define PATH "/api/v1/write"
+#define PORT 9090
